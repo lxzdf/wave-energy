54d084594a206dc4b84a454f4695a2f45c33c78e
lxz <2685ghtrs@gmail.com>
2025-12-08 11:59:05 +0800

    第一题一问，貌似完成了

--- Q1/question_01.py
+++ Q1/question_01.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.f2py.crackfortran import true_intent_list
 
 # 浮子与振子在初始时刻的状态
 x_fu = 0.0              # 浮子的位移
@@ -27,47 +28,69 @@ def wave_force(t):
     return 6250*np.cos(wave_fn*t)
 
 # 静水恢复力的计算
-def water_force(t):
-    return 1025*9.8*v_fu*t
+def water_force(x_fu):
+    if x_fu>0 and x_fu<=0.8:
+        return (((x_fu/0.8)**2)*np.pi*x_fu/3)*1025*9.8
+    elif x_fu>0.8 and x_fu<=2.8:
+        return (np.pi*1*1*(x_fu-0.8) + np.pi*1*1*0.8/3)*1025*9.8
+    else:
+        return (np.pi*1*1*2 + np.pi*1*1*0.8/3)*1025*9.8
 
 # 计算浮子的加速度
 def acc_fu(time, x_fu, v_fu, x_zh, v_zh):
-    return (wave_force(time) - c_wave*v_fu - c_pto*(v_fu-v_zh) - water_force()-k*(x_fu-x_zh))/(m_fu + m_fu_add)
+    return (wave_force(time) - c_wave*v_fu - c_pto*(v_fu-v_zh) - water_force(x_fu)-k*(x_fu-x_zh))/(m_fu + m_fu_add)
 
 # 计算振子的加速度
 def acc_zh(time, x_fu, v_fu, x_zh, v_zh):
-    return 0-c_pto*(v_fu-v_zh)-k*(x_fu-x_zh)/m_zhen
+    return (0-c_pto*(v_fu-v_zh)-k*(x_fu-x_zh))/m_zhen
 
 
-# 浮子的速度(L)、加速度(K)----[t+Δt时刻]
-fu_K1 = acc_fu(time, x_fu, v_fu, x_zh, v_zh)
-fu_L1 = v_fu                                                                                                # 使用t时刻的速度
-# 振子的速度(L)、加速度(K)----[t+Δt时刻]
-zh_K1 = acc_zh(time, x_fu, v_fu, x_zh, v_zh)
-zh_L1 = v_zh
+def rk4(time, x_fu, v_fu, x_zh, v_zh):
+    # 浮子的速度(L)、加速度(K)----[t+Δt时刻]
+    fu_K1 = acc_fu(time, x_fu, v_fu, x_zh, v_zh)
+    fu_L1 = v_fu                                                                                                # 使用t时刻的速度
+    # 振子的速度(L)、加速度(K)----[t+Δt时刻]
+    zh_K1 = acc_zh(time, x_fu, v_fu, x_zh, v_zh)
+    zh_L1 = v_zh
 
 
-fu_K2 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1)
-fu_L2 = v_fu + 0.5*dt*fu_K1
-zh_K2 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1)
-zh_L2 = v_zh + 0.5*dt*zh_K1
+    fu_K2 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1)
+    fu_L2 = v_fu + 0.5*dt*fu_K1
+    zh_K2 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1)
+    zh_L2 = v_zh + 0.5*dt*zh_K1
 
 
-fu_K3 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2)
-fu_L3 = v_fu + 0.5*dt*fu_K2
-zh_K3 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2)
-zh_L3 = v_zh + 0.5*dt*zh_K2
+    fu_K3 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2)
+    fu_L3 = v_fu + 0.5*dt*fu_K2
+    zh_K3 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2)
+    zh_L3 = v_zh + 0.5*dt*zh_K2
 
 
-fu_K4 = acc_fu(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3)
-fu_L4 = v_fu + fu_K3*dt
-zh_K4 = acc_zh(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3)
-zh_L4 = v_zh + zh_K3*dt
+    fu_K4 = acc_fu(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3)
+    fu_L4 = v_fu + fu_K3*dt
+    zh_K4 = acc_zh(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3)
+    zh_L4 = v_zh + zh_K3*dt
 
 
-# 计算得到t+Δt时刻的浮子、振子的速度与位移
-v_fu = v_fu + dt*(fu_K1 + 2*fu_K2 + 2*fu_K3 + fu_K4)/6
-x_fu = x_fu + dt*(fu_L1 + 2*fu_L2 + 2*fu_L3 + fu_L4)/6
+    # 计算得到t+Δt时刻的浮子、振子的速度与位移
+    v_fu_new = v_fu + dt*(fu_K1 + 2*fu_K2 + 2*fu_K3 + fu_K4)/6
+    x_fu_new = x_fu + dt*(fu_L1 + 2*fu_L2 + 2*fu_L3 + fu_L4)/6
 
-v_zh = v_zh + dt*(zh_K1 + 2*zh_K2 + 2*zh_K3 + zh_K4)/6
-x_zh = x_zh + dt*(zh_L1 + 2*zh_L2 + 2*zh_L3 + zh_L4)/6
+    v_zh_new = v_zh + dt*(zh_K1 + 2*zh_K2 + 2*zh_K3 + zh_K4)/6
+    x_zh_new = x_zh + dt*(zh_L1 + 2*zh_L2 + 2*zh_L3 + zh_L4)/6
+
+    return v_fu_new, x_fu_new, v_zh_new, x_zh_new
+
+
+t_list = []
+x_fu_list, v_fu_list, x_zh_list, v_zh_list = [], [], [], []
+while time <= end:
+    v_fu, x_fu, v_zh, x_zh = rk4(time, x_fu, v_fu, x_zh, v_zh)
+    t_list.append(time)
+    x_fu_list.append(x_fu)
+    v_fu_list.append(v_fu)
+    x_zh_list.append(x_zh)
+    v_zh_list.append(v_zh)
+    time = time + dt
+
+# 写入数据
