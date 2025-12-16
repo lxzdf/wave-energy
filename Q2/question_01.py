4da66e9339abac0e531137393fcc641352add818
lxz <2685ghtrs@gmail.com>
2025-12-16 14:58:33 +0800

    更新了第二问第一题的代码

--- Q2/question_01.py
+++ Q2/question_01.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 此为第二题的第一问，使用四阶龙格库塔法+变步长求解
+# 此为第二题的第一问，使用四阶龙格库塔法
 import numpy as np
 
 # 浮子与振子在初始时刻的状态
@@ -16,12 +16,10 @@ k = 80000                                         # pto弹簧刚度
 c_wave = 167.8395                                 # 波浪的兴波阻尼系数
 # c_pto                                 --------- # pto的阻尼系数,这个是代求值
 
-time = 0                                          # 开始时间
 dt = 0.01                                        # 时间步长
 end = 500                                         # 结束时间
-
+power_start = 400.0
 # 假设400s之后是稳定的，在计算400s到500s之间的平均功率
-
 # 波浪激励力的计算
 def wave_force(run_time):
     return 4890*np.cos(wave_fn*run_time)
@@ -48,56 +46,44 @@ def acc_zh(time, x_fu, v_fu, x_zh, v_zh, c_pto):
     return (c_pto*(v_fu-v_zh)+k*(x_fu-x_zh))/m_zhen
 
 
-def rk4(time, x_fu, v_fu, x_zh, v_zh, c_pto):
-    # 浮子的速度(L)、加速度(K)----[t+Δt时刻]
-    fu_K1 = acc_fu(time, x_fu, v_fu, x_zh, v_zh, c_pto)
-    fu_L1 = v_fu                                                                                                # 使用t时刻的速度
-    # 振子的速度(L)、加速度(K)----[t+Δt时刻]
-    zh_K1 = acc_zh(time, x_fu, v_fu, x_zh, v_zh, c_pto)
-    zh_L1 = v_zh
+# 等价的方法
+def deriv(run_time, y, c_pto):
+    x_fu = y[0]
+    v_fu = y[1]
+    x_zh = y[2]
+    v_zh = y[3]
+    a_fu = acc_fu(run_time, x_fu, v_fu, x_zh, v_zh, c_pto)
+    a_zh = acc_zh(run_time, x_fu, v_fu, x_zh, v_zh, c_pto)
 
-    fu_K2 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1, c_pto)
-    fu_L2 = v_fu + 0.5*dt*fu_K1
-    zh_K2 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L1, v_fu+0.5*dt*fu_K1, x_zh+0.5*dt*zh_L1, v_zh+0.5*dt*zh_K1, c_pto)
-    zh_L2 = v_zh + 0.5*dt*zh_K1
+    return np.array([v_fu, a_fu, v_zh, a_zh], dtype=float)
 
-    fu_K3 = acc_fu(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2, c_pto)
-    fu_L3 = v_fu + 0.5*dt*fu_K2
-    zh_K3 = acc_zh(time+0.5*dt, x_fu+0.5*dt*fu_L2, v_fu+0.5*dt*fu_K2, x_zh+0.5*dt*zh_L2, v_zh+0.5*dt*zh_K2,c_pto)
-    zh_L3 = v_zh + 0.5*dt*zh_K2
-
-    fu_K4 = acc_fu(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3, c_pto)
-    fu_L4 = v_fu + fu_K3*dt
-    zh_K4 = acc_zh(time+dt, x_fu+dt*fu_L3, v_fu+dt*fu_K3, x_zh+dt*zh_L3, v_zh+dt*zh_K3, c_pto)
-    zh_L4 = v_zh + zh_K3*dt
-
-    # 计算得到t+Δt时刻的浮子、振子的速度与位移
-    v_fu_new = v_fu + dt*(fu_K1 + 2*fu_K2 + 2*fu_K3 + fu_K4)/6
-    x_fu_new = x_fu + dt*(fu_L1 + 2*fu_L2 + 2*fu_L3 + fu_L4)/6
-
-    v_zh_new = v_zh + dt*(zh_K1 + 2*zh_K2 + 2*zh_K3 + zh_K4)/6
-    x_zh_new = x_zh + dt*(zh_L1 + 2*zh_L2 + 2*zh_L3 + zh_L4)/6
-
-    return v_fu_new, x_fu_new, v_zh_new, x_zh_new
+# 四阶龙格库塔法主要做的事情是，给定当前时刻的t以及当前时刻的系统状态，使用四阶龙格库塔法将此状态推进一个步长，得到下一时刻的状态
+def rk4_step(t, y, h, c_pto):
+    k1 = deriv(t, y, c_pto)
+    k2 = deriv(t+0.5*h, y+0.5*h*k1, c_pto)
+    k3 = deriv(t+0.5*h, y+0.5*h*k2, c_pto)
+    k4 = deriv(t+h, y+h*k3, c_pto)
+    return y + h*(k1 + 2*k2 + 2*k3 + k4)/6
 
 
 def cal_power(c_pto):
     count = 0   # 时间步
     power_sum = 0.0 # 功率
-    time = 0.0
-    x_fu = 0.0
-    v_fu = 0.0
-    x_zh = 0.0
-    v_zh = 0.0
 
-    while time <= end:
-        v_fu, x_fu, v_zh, x_zh = rk4(time, x_fu, v_fu, x_zh, v_zh, c_pto)
-        time = time + dt
-        if time >= 400:
-            power_sum = power_sum + c_pto * (v_fu-v_zh)*(v_fu-v_zh)
+    y = np.array([0.0, 0.0, 0.0, 0.0], dtype=float)
+    steps = int(np.floor(end / dt))
+    time = 0.0
+    for zz in range(steps):
+        y = rk4_step(time, y, dt, c_pto)
+        time = time+dt
+
+        if time >= power_start:
+            pp = c_pto*pow((y[1]-y[3]), 2)
+            power_sum = power_sum + pp
             count = count + 1
 
-    return power_sum/count
+    return power_sum / count
+
 
 if __name__ == '__main__':
 
